1e312544c3aa78922af6e19eae1519f69581915e
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-01-07 20:29:01 +0200

    feat: initial setup

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Home Page</title>
+        <meta name="description" content="Next page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header></header>
+      <main className="px-4 py-2">{children}</main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Layout;
